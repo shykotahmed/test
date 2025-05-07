119b9ada653cd7587dba35a9e81ded618c8c2007
Shikut Ahammed <52927710+shykotahmed@users.noreply.github.com>
2025-05-07 17:25:44 +0600

    Add files via upload

---
+++ p1.php
@@ -0,0 +1,73 @@
+<?php
+/*PHP Mini Projects
+Project 1: Number Classifier
+Make a PHP script that checks a number. First, check if the number is positive or negative. Then, check if the number is even or odd.
+
+Take input or define variable
+Show output like: "The number -4 is Negative and Even".*/
+
+
+$number = 15;
+//negative, Positive
+
+if ($number > 0) {
+    echo $numSign = "Positive";
+}elseif ($number <0) {
+    echo $numSign = "Negative";   
+}else {
+    echo $number = "Zero";
+}
+
+//Odd, Even 
+if ($number /2 == 0){
+    echo $oddEven = "Even";
+} else {
+    echo $numSign = "Odd";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+////git tets
+
+
+
+
+
+
+// $number = 2;
+
+// //check the number is positive or negative
+// if ($number > 0) {
+//     echo $numSign = "Positive";
+// } elseif ($number < 0) {
+//     echo $numSign = "Negative";
+// } else {
+//     echo $number = "Zero";
+// }
+
+// //check the number is even or odd.
+
+// if ($number / 2 == 0) {
+//     echo $oddEven = "Even";
+// } else {
+//     echo "Odd";
+// }
+
+
+
+
+
+
+
+
+?>
